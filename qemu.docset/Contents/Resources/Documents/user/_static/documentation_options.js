7edcc91e93a66bbe1e31cbbe1687676f8db83df3
Matheus Catarino França <matheus-catarino@hotmail.com>
2020-10-19 12:12:17 -0300

    Docset

---
+++ qemu.docset/Contents/Resources/Documents/user/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '4.2.92 (v5.0.0-rc2-42-g2f7cc1fbd6)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
